91ebcd3fb8956b0f0d42eb299a907a0157d6f8b5
TrungNgo21 <ngolambaotrung@gmail.com>
2023-12-29 09:45:38 +0700

    feat: set-up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,6 +17,10 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -36,4 +40,51 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation("androidx.cardview:cardview:1.0.0")
+
+    implementation("com.squareup.picasso:picasso:2.71828")
+
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:32.5.0"))
+    // TODO: Add the dependencies for Firebase products you want to use
+    // When using the BoM, don't specify versions in Firebase dependencies
+    implementation("com.google.firebase:firebase-analytics")
+
+    compileOnly("org.projectlombok:lombok:1.18.30")
+
+    annotationProcessor("org.projectlombok:lombok:1.18.30")
+
+    implementation("com.google.firebase:firebase-auth")
+
+    implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.firebase:firebase-storage:17.0.0")
+
+
+    implementation("androidx.fragment:fragment:1.5.7")
+
+//    implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.8.0"))
+
+
+    implementation("com.facebook.shimmer:shimmer:0.5.0")
+
+    implementation("com.intuit.sdp:sdp-android:1.0.6")
+
+    implementation("com.makeramen:roundedimageview:2.3.0")
+
+    implementation("com.google.code.gson:gson:2.8.8")
+    implementation("androidx.multidex:multidex:2.0.1")
+
+    implementation("me.relex:circleindicator:2.1.6")
+
+    implementation("org.slf4j:slf4j-simple:1.7.25")
+
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+
+    implementation("com.google.maps:google-maps-services:2.2.0")
+    implementation("com.google.android.libraries.places:places:3.2.0")
+
+    implementation("com.google.maps.android:android-maps-utils:2.4.0")
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
+    implementation("com.google.android.gms:play-services-location:21.0.1")
 }
